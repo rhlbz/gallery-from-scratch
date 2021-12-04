87526afdce1da50119a502423184da939bf9ffd2
rhlbz <95280275+rhlbz@users.noreply.github.com>
2021-12-04 20:56:26 +0100

    public 2

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:rhlbz/gallery-from-scratch.git public:gh-pages
+git push -f git@github.com:rhlbz/gallery-from-scratch.git master:gh-pages
 cd -
