ce69f3ba31b5247e68d5aee344a958437f5d7f41
rhlbz <95280275+rhlbz@users.noreply.github.com>
2021-12-05 18:07:54 +0100

    end mobile

--- src/components/gallery/GalleryControls.vue
+++ src/components/gallery/GalleryControls.vue
@@ -47,55 +47,24 @@
     border: none;
     transition: background 0.4s linear, color 0.4s linear;
 }
-.control.info-control{
-    background: rgba(50, 75, 74, .7);
-    display: flex;
-    z-index: 2;
-    top: 92.5%;
-    width: 7.5%;
-    font-size: 2em;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
-    height: 10%;
-    position: absolute;
-    color: white;
-    border: none;
-    transition: background 0.4s linear, color 0.4s linear;
-}
 .control.gallery-controls:hover{
     cursor: pointer;
     color: #003A3D;
     background: rgba(0, 201, 200, .7);
 }
-.control.preview-controls{
-    background: rgba(50, 75, 74, .7);
-    display: flex;
-    z-index: 2;
-    width: 2%;
-    font-size: 1.5em;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    color: white;
-    border: none;
-    transition: background 0.8s linear, color 0.8s linear;
-}
-.control.preview-controls:hover{
-    cursor: pointer;
-    color: #003A3D;
-    background: rgba(0, 201, 200, .7);
-}
+
 .next-control.gallery-controls{
     left: 92.5%;
 }
-.next-control.preview-controls {
-    left: 67%;
-}
 
+@media screen and (min-width: 320px) and (max-width:  768px){
+    .control.gallery-controls{
+        top: 0;
+        height: 100%;
+        width: 5%;
+    }
+    .next-control.gallery-controls{
+        left: 95%;
+    }
+}
 </style>
